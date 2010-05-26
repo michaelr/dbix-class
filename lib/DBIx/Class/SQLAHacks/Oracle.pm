5cc0b80437aa50a18b7c7f52f4b2a1e3d1f3e5f6
Alexander Hartmaier <abraxxa@cpan.org>
2010-05-26 16:40:29 +0000

    added _generate_bind_param_name method to SQLAHacks::Oracle

--- lib/DBIx/Class/SQLAHacks/Oracle.pm
+++ lib/DBIx/Class/SQLAHacks/Oracle.pm
@@ -7,6 +7,13 @@ use Carp::Clan qw/^DBIx::Class|^SQL::Abstract/;
 #
 # Oracle has a different INSERT...RETURNING syntax
 #
+
+sub _generate_bind_param_name {
+  my ($self, $colname) = @_;
+
+  return ":$colname";
+}
+
 sub _insert_returning {
   my ($self, $fields) = @_;
 
@@ -17,9 +24,9 @@ sub _insert_returning {
   });
   
   my $bind_f = $self->_SWITCH_refkind($fields, {
-    ARRAYREF     => sub {join ', ', map { $self->_quote(":$_") } @$fields;},
-    SCALAR       => sub {$self->_quote(":$fields")},
-    SCALARREF    => sub {":$$fields"},
+    ARRAYREF     => sub {join ', ', map { $self->_generate_bind_param_name($_) } @$fields;},
+    SCALAR       => sub {$self->_generate_bind_param_name($fields)},
+    SCALARREF    => sub {$self->_generate_bind_param_name($$fields)},
   });
   
   return join (' ', $self->_sqlcase(' returning'), $f, $self->_sqlcase('into'), $bind_f);
