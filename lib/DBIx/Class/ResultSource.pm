33a50ca49550a2f21ed375d9215c420c4c068aff
Daniel Ruoso <daniel@ruoso.com>
2010-06-02 17:27:37 +0000

    Most of the code reviewed... Missing ResultSet->populate, ResultSet->find, Row->copy and Relationship::Accessor->add_relationship_accessor

--- lib/DBIx/Class/ResultSource.pm
+++ lib/DBIx/Class/ResultSource.pm
@@ -1440,12 +1440,7 @@ sub _resolve_condition {
       $self->throw_exception ('Unable to determine relationship name for condition resolution');
     }
 
-    $cond = $cond->(
-      $for,
-      ref $for ? 'me' : $as,
-      $self,
-      $rel,
-    );
+    return $cond->($for, ref $for ? 'me' : $as, $self, $rel, ref $for ? $for : undef);
 
   } elsif (ref $cond eq 'HASH') {
     my %ret;
